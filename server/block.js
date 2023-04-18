52ce539050e4e2d1c5f43f2dc65e0022a96999a7
PHOOMCHANOK CHAYANON <77683049+OniiChan00@users.noreply.github.com>
2023-04-18 21:51:39 +0700

    Update block.js

--- server/block.js
+++ server/block.js
@@ -1,11 +1,12 @@
 const SHA256 = require('crypto-js/sha256');
 class Block{
-    constructor(index, timestamp,item,buyer,seller, previousHash = ''){
+    constructor(index, timestamp,item,item_id,buyer,seller, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
         this.item = item;
         this.buyer = buyer;
         this.seller = seller;
+        this.item_id = item_id;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
